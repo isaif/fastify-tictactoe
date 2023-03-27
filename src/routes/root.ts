c434d382ff2b8de7f6d7208bf0eebd7cb1a04ab2
isaif <saif935775@gmail.com>
2023-03-27 22:49:15 +0530

    feat(route): add new route to get player id

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,35 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
+import fastifyCors from "@fastify/cors";
+import { v4 as genId } from "uuid";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.register(fastifyCors, {
+    origin: "http://localhost:3000",
+    methods: ["GET"],
+  });
+
+  fastify.get("/playerId", async (req, res) => {
+    const playerId = genId();
+    res.send({ playerId });
+  });
+
+  // fastify.register(fastifyWebsocket);
+  // fastify.register(async function (fastify) {
+  //   fastify.get(
+  //     "/chat",
+  //     { websocket: true },
+  //     (connection /* SocketStream */, req /* FastifyRequest */) => {
+  //       connection.socket.on("message", (message) => {
+  //         // message.toString() === 'hi from client'
+  //         connection.socket.send(`hi from server ${message}`);
+  //       });
+  //     }
+  //   );
+  // });
+
+  // fastify.get("/", async function (request, reply) {
+  //   return {};
+  // });
+};
 
 export default root;
